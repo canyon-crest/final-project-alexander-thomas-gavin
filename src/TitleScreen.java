aa9735209a567a6dcb320ad88ac2eb244da0de43
bridgemanG <bridgemang8033@my.sduhsd.net>
2025-05-17 13:51:57 -0700

    fixed spelling error

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -4,7 +4,7 @@ public class TitleScreen extends JFrame {
 	public static final int WIDTH = 1920;
 	public static final int HEIGHT = 1080;
 	public TitleScreen() {
-		super("Arhangel");
+		super("Archangel");
 		setSize(WIDTH,HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
