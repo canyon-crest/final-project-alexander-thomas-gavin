0ae0b7556c29c6cc043b50fc7e7a5c454b1f560b
OreoBean <110355616+OreoBean@users.noreply.github.com>
2025-05-19 08:40:52 -0700

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,25 @@
 
-public class Character {
-
+public class Character extends Entity {
+  private int maxHealth;
+  private int health;
+  public Character(double xLocation, double yLocation, double friction, int width, int height, int health){
+    super(xLocation, yLocation, friction, width, height, 0);
+    this.health = health;
+    this.maxHealth = health;
+  }
+  public int getHealth(){
+    return health;
+  }
+  public int getMaxHealth(){
+    return maxHealth;
+  }
+  public void takeDamage(int amount){
+    health -= amount;
+    if(health < 0){
+      health = 0;
+    }
+    if(health > maxHealth){
+      health = maxHealth;
+    }
+  }
 }
